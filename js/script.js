772011ce7a73816d747dd9160f8d97be4a5d0c19
Lech Cirmirakis <lech.cirmirakis@go2.pl>
2017-10-31 19:30:23 +0100

    add jquery script to change fixed_navbar

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
 $(document).ready(function(){
 
-		// $(window).scroll(function() {
-		//
-		// 	if ($(window).scrollTop() < 80 ) {
-		//
-		// 	}
-		//
-		//
-		//
-		//
-		// });
-		//
-		//
-		//
+	$(window).scroll(function() {
 
+		if ($(window).scrollTop() > 100 ) {
+			$('.fixed_nav').css ({
+				'height': '60px',
+				'background-color': 'rgba(249, 249, 251, 0.8)'
+			});
 
+			$('.main_logo').css ({
+			'margin-top': '4px'
+			});
 
+			$('.fixed_nav ul').css ({
+			'margin-top': '22px'
+			});
 
+		}
 
+		else {
+			$('.fixed_nav').css ({
+			'height': '100px',
+			'background-color': 'transparent'
+			});
+
+			$('.main_logo').css ({
+			'margin-top': '22px'
+			});
+			
+			$('.fixed_nav ul').css ({
+			'margin-top': '38px'
+			});
+		}
+	});
 
 	// $('.nav-icon').click(function(){
 	// 	$(this).toggleClass('open');
